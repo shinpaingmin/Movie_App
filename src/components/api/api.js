118f564690880da20bbc6ade774d1b6c4d6ba356
shinpaingmin <103930442+shinpaingmin@users.noreply.github.com>
2023-07-29 00:29:48 +0630

    Update api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -4,4 +4,4 @@ export const api = axios.create({
     baseURL : "https://api.themoviedb.org/3",
 })
 
-export const api_key = "50ab14447d3e351ad4e922cc4f44ce36";
+export const api_key = "generate_your_own_api_key";
